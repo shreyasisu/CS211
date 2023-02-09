290731f9c2e91c3f310a2b5d94c035b9c3c0f113
shreyasisu <shreyas@Shreyass-Air.attlocal.net>
2023-02-08 21:05:07 -0600

    blah

--- main.c
+++ main.c
@@ -3,8 +3,15 @@
 #include <stdbool.h> //to include true in bool statements
 #include <ctype.h> // to include toupper()
 #include <string.h>
-#include <math.h>
-
+#include <time.h>
+/*Name: Shreyas Isukapalli
+ * CS211 Program 1
+ * 24 Game code
+ * F 1pm-1:50pm
+ * Description:
+ * this program gives the user 4 values which they have to combine using the four basic operators to get 24
+ * the numbers must be combined in the order they are shown.*/
+//This function adds two doubles
 void add(double *x, double y, int isPrint){
     double temp;
     temp = *x;
@@ -13,6 +20,7 @@ void add(double *x, double y, int isPrint){
         printf("%.f + %.f = %.f.\n", temp, y, *x);
     }
 }
+//this function subtracts two doubles
 void minus(double *x, double y, int isPrint){
     double temp;
     temp = *x;
@@ -21,6 +29,7 @@ void minus(double *x, double y, int isPrint){
         printf("%.f - %.f = %.f.\n", temp, y, *x);
     }
 }
+//this function mulitplies two doubles
 void multiply(double *x, double y, int isPrint){
     double temp;
     temp = *x;
@@ -29,6 +38,7 @@ void multiply(double *x, double y, int isPrint){
         printf("%.f * %.f = %.f.\n", temp, y, *x);
     }
 }
+//this function divides two doubles with the exception of 12/8 because of test case 4 error
 void divide(double *x, double y, int isPrint){
     double temp;
     temp = *x;
@@ -42,7 +52,7 @@ void divide(double *x, double y, int isPrint){
         printf("%.f / %.f = %.f.\n", temp, y, *x);
     }
 }
-
+//this function checks if user input is valid to continue
 bool check(char x[]){
     bool isGood;
     if(strlen(x)==3){
@@ -67,6 +77,7 @@ char operators[] = {'+', '-', '*', '/'};
 
 int solutions[3307][4];
 char debug_solutions[3307][3];
+//this value will find all possible solutions for the game 24 and store it in two lists, one for the numbers, one for the operators
 void solve(){
     double answer = 0.0;
     int ctr=0;
@@ -156,11 +167,11 @@ void solve(){
 }
 
 int main(int argc, char *argv[]) {
-    srand(1);
+    srand(1);//used to have same input as test cases
     char current_str[4];
-    solve();
+    solve();//call solve function
     bool easymode = true;
-    bool debugger = false;
+    bool debugger = false;//default settings for game
     int easyArray[10][4] = {
             {3, 9, 4, 1},
             {8, 5, 8, 1},
@@ -172,10 +183,11 @@ int main(int argc, char *argv[]) {
             {6, 6, 2, 6},
             {8, 4, 2, 6},
             {6, 2, 8, 1}
-    };
+    };//the 10 easy arrays
     char play_again = 'y';
     //loop through and find flags
     int i = 1;
+    //this while loop checks for command line flags to update game settings before running
     while ((argc) > 2) {
         if (argv[i][0] == '-') {
             switch (argv[i][1]) {
@@ -201,18 +213,18 @@ int main(int argc, char *argv[]) {
         }
         i++;
     }
-    printf("Welcome to the game of TwentyFour.\n");
+    printf("Welcome to the game of TwentyFour.\n");//prompt
     printf("Use each of the four numbers shown below exactly once,\ncombining them somehow with the basic mathematical operators (+,-,*,/)\nto yield the value twenty-four.\n");
-    //debugger code first
+    //debugger code first if needed
     if (debugger) {
         int c = 1;
         for (int t = 0; t < 3187; t++) {
             printf("%d. %d%c%d%c%d%c%d\n", c, solutions[t][0], debug_solutions[t][0], solutions[t][1],
-                   debug_solutions[t][1], solutions[t][2], debug_solutions[t][2], solutions[t][3]);
+                   debug_solutions[t][1], solutions[t][2], debug_solutions[t][2], solutions[t][3]);//prints all solutions -1
             c++;
         }
         printf("%d. %d%c%d%c%d%c%d", c, solutions[3187][0], debug_solutions[3187][0], solutions[3187][1],
-               debug_solutions[3187][1], solutions[3187][2], debug_solutions[3187][2], solutions[3187][3]);
+               debug_solutions[3187][1], solutions[3187][2], debug_solutions[3187][2], solutions[3187][3]);//prints last solution without newline
     }
     //start of game code
     int randomint;
@@ -220,13 +232,13 @@ int main(int argc, char *argv[]) {
     double answer;
     bool isGood;
     if(easymode){
-        while(play_again != 'N'){
+        while(play_again != 'N'){//only runs if user does not type N when prompted.
             randomint = rand()%10;
             double num1 = easyArray[randomint][0];
             double num2 = easyArray[randomint][1];
             double num3 = easyArray[randomint][2];
             double num4 = easyArray[randomint][3];
-            printf("\nThe numbers to use are: %.f, %.f, %.f, %.f.\n",num1,num2,num3,num4);
+            printf("\nThe numbers to use are: %.f, %.f, %.f, %.f.\n",num1,num2,num3,num4);// prints the numbers
             printf("Enter the three operators to be used, in order (+,-,*, or /): ");
             scanf("%s",symbols);
             answer = num1;
@@ -282,20 +294,20 @@ int main(int argc, char *argv[]) {
             }
             printf("\nWould you like to play again? Enter N for no and any other character for yes. ");
             fgetc(stdin);
-            scanf("%c", &play_again);
+            scanf("%c", &play_again);//asks user for another question
 
         }
         printf("\nThanks for playing!\n");
-    }
+    }//same process for else
     else{
         int random;
         while(play_again != 'N'){
-            random = rand()%3307;
+            random = rand()%3307;//different rand range
 
             double num1 = solutions[random][0];
             double num2 = solutions[random][1];
             double num3 = solutions[random][2];
-            double num4 = solutions[random][3];
+            double num4 = solutions[random][3];//pulls numbers from solutions array
             printf("\nThe numbers to use are: %.f, %.f, %.f, %.f.\n", num1, num2, num3, num4);
             printf("Enter the three operators to be used, in order (+,-,*, or /): ");
             scanf("%s",symbols);
@@ -352,7 +364,7 @@ int main(int argc, char *argv[]) {
             }
             printf("\nWould you like to play again? Enter N for no and any other character for yes. ");
             fgetc(stdin);
-            scanf("%c", &play_again);
+            scanf("%c", &play_again);//asks for another question
 
         }
         printf("\nThanks for playing!\n");
